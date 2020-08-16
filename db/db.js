1669ea09ab2eb5bf4785540528759690cd56e64e
aibr24 <aliibrahim4793@gmail.com>
2020-08-16 15:35:04 +0300

    deployment Adds

--- db/db.js
+++ db/db.js
@@ -1,11 +1,24 @@
 const { sequelize, Sequelize } = require("sequelize");
 
-const db = new Sequelize({
-  username: "postgres",
-  password: "A619916a",
-  database: "gameshopdb",
-  dialect: "postgres",
-  host: "localhost",
-});
+// const db = new Sequelize({
+//   username: "postgres",
+//   password: "A619916a",
+//   database: "gameshopdb",
+//   dialect: "postgres",
+//   host: "localhost",
+// });
 
+const db = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+    })
+  : new Sequelize({
+      username: "postgres",
+      password: "A619916a",
+      database: "gameshopdb",
+      dialect: "postgres",
+      host: "localhost",
+      logging: false,
+    });
 module.exports = db;
